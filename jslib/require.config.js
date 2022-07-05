339a796586a26d53c2683f5b29b378f8beab53d3
jun4rui <jun4rui@gmail.com>
2022-07-05 15:00:24 +0800

    Init.

---
+++ jslib/require.config.js
@@ -0,0 +1,9 @@
+requirejs.config({
+	baseUrl: 'node_modules',
+	paths:{
+		jquery:'jquery/dist/jquery.min',
+		knockoutDebug:'knockout/build/output/knockout-latest.debug',
+		knockout:'knockout/build/output/knockout-latest',
+		dayjs:'dayjs/dayjs.min'
+	}
+});
